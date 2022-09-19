642bb680a328c445388a3f029486b59bdfda4e9d
Willie <carranzawillie@gmail.com>
2022-09-19 16:07:39 -0500

    Added .env file and updated .gitignore file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // mongodb+srv://grimhoney:linkStart91@cluster-1.aoqnihm.mongodb.net/inventory?retryWrites=true&w=majority
-
+const dotenv = require('dotenv');
+dotenv.config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -16,8 +17,7 @@ const helmet = require('helmet');
 var app = express();
 
 const mongoose = require('mongoose');
-const dev_db_url = "mongodb+srv://grimhoney:linkStart91@cluster-1.aoqnihm.mongodb.net/inventory?retryWrites=true&w=majority";
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
